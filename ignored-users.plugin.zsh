61f9f861570bb6b236943f69e7747b388e7d6959
Grigorii Horos <horosgrisa@gmail.com>
2020-02-16 20:14:38 +0200

    Add cache

--- ignored-users.plugin.zsh
+++ ignored-users.plugin.zsh
@@ -1,3 +1,10 @@
 if [[ -e /etc/passwd ]]; then
-  zstyle ':completion:*:*:*:users' ignored-patterns $(cat /etc/passwd | awk -F':' '($3<1000 && $3>0)||$3>10000{print $1}' | xargs)
+  cache_file="${TMPDIR:-/tmp}/zpm-zsh-ignored-users-cache.$UID.zsh"
+  if [[ /etc/passwd -nt "$cache_file" || ! -s "$cache_file"  ]]; then
+    ignored=( $(cat /etc/passwd | awk -F':' '($3<1000 && $3>0)||$3>10000{print $1}' | xargs) )
+    zstyle ':completion:*:*:*:users' ignored-patterns $ignored
+    echo "zstyle ':completion:*:*:*:users' ignored-patterns $ignored" >! "$cache_file" 2> /dev/null
+  else
+    source "$cache_file"
+  fi
 fi
