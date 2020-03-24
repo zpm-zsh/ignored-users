66a810fd4e77508d103a1865935084d2ef8ebce1
Grigorii Horos <horosgrisa@gmail.com>
2020-03-24 02:16:46 +0200

    optimize

--- ignored-users.plugin.zsh
+++ ignored-users.plugin.zsh
@@ -1,6 +1,6 @@
 if [[ -e /etc/passwd ]]; then
   CACHE_FILE="${TMPDIR:-/tmp}/zsh-${UID}/ignored-users.zsh"
-  if [[ /etc/passwd -nt "$CACHE_FILE" || ! -s "$CACHE_FILE"  ]]; then
+  if [[ ! "$CACHE_FILE" -nt /etc/passwd ]]; then
     ignored=( $(cat /etc/passwd | awk -F':' '($3<1000 && $3>0)||$3>10000{print $1}' | xargs) )
     zstyle ':completion:*:*:*:users' ignored-patterns $ignored
     echo "zstyle ':completion:*:*:*:users' ignored-patterns $ignored" >! "$CACHE_FILE" 2> /dev/null
