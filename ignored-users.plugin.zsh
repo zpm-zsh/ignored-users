b41d79332e7c1b31261929649b7e923dbd092c68
Grigorii Horos <horosgrisa@gmail.com>
2020-04-24 14:55:37 +0300

    Fix compatibility with zsh<=5.4

--- ignored-users.plugin.zsh
+++ ignored-users.plugin.zsh
@@ -1,9 +1,11 @@
 if [[ -e /etc/passwd ]]; then
   CACHE_FILE="${TMPDIR:-/tmp}/zsh-${UID}/ignored-users.zsh"
-  source "$CACHE_FILE" 2>/dev/null || {
+  if [[ -f "$CACHE_FILE" ]]; then
+    source "$CACHE_FILE"
+  else
     ignored=( $(cat /etc/passwd | awk -F':' '($3<1000 && $3>0)||$3>10000{print $1}' | xargs) )
     zstyle ':completion:*:*:*:users' ignored-patterns $ignored
     echo "zstyle ':completion:*:*:*:users' ignored-patterns $ignored" >! "$CACHE_FILE" 2> /dev/null
     zcompile "$CACHE_FILE"
-  }
+  fi
 fi
